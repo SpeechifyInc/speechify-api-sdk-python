9c3f68fafeeadd9917dd0f99b2466528837b10b3
fern-api <115122769+fern-api[bot]@users.noreply.github.com>
2025-03-20 03:47:45 +0000

    Release 0.0.141

--- src/speechifyinc/core/client_wrapper.py
+++ src/speechifyinc/core/client_wrapper.py
@@ -22,7 +22,7 @@ class BaseClientWrapper:
         headers: typing.Dict[str, str] = {
             "X-Fern-Language": "Python",
             "X-Fern-SDK-Name": "speechify-api",
-            "X-Fern-SDK-Version": "0.0.139",
+            "X-Fern-SDK-Version": "0.0.141",
         }
         headers["Authorization"] = f"Bearer {self._get_token()}"
         return headers
